89290827656b3a7fd798d93b0e128c676a3a056a
andry-z <andryzotti051@gmail.com>
2021-03-25 23:41:11 +0100

    Pagine con card

---
+++ app/views/layouts/script.js
@@ -0,0 +1,12 @@
+/*bulmaCarousel.attach('#slider', {
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    infinite: true,
+  });
+
+  bulmaCarousel.attach('#carousel-demo', {
+    slidesToScroll: 1,
+    slidesToShow: 4
+    });
+
+    */
