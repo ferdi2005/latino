f219a7bc3e28f99c832758aff5a48c0596eec89d
ferdi2005 <ferdi.traversa@gmail.com>
2021-03-26 10:40:39 +0100

    Risolvo errore user non loggato
    Comportamento differente per utente loggato o  non loggato

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   def home
     @study = Study.all.sample 
     @studies = Study.all.shuffle
-    suitable_quizzes = Quiz.find(Quiz.ids - current_user.user_quizzes.where(surrender: false).pluck(:quiz_id))
+    if user_signed_in?
+      suitable_quizzes = Quiz.find(Quiz.ids - current_user.user_quizzes.where(surrender: false).pluck(:quiz_id))
+    else
+      suitable_quizzes = Quiz.all
+    end
     @quizzes = suitable_quizzes.shuffle
   end
 
